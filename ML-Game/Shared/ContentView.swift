130d315f4c9315943bbe1ce617e32948d707d96d
Damir Yumaguzhin <d.yumaguzhin@andersenlab.com>
2022-10-12 15:02:09 +0300

    MLG-0: hello world

--- ML-Game/Shared/ContentView.swift
+++ ML-Game/Shared/ContentView.swift
@@ -9,17 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button {
-                
-            } label: {
-                Text("Нажми на меня")
-            }
-            ZStack {
-                RoundedRectangle(cornerRadius: 20).fill(Color.blue).offset(y: 30).shadow(radius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
-                Text("Никита пидр")
-            }
-        }
+        Text("Hello World!")
     }
 }
 
